e3062df4afa1a94e8f86b4d12dba069e34e5f490
NAQuantum <45130165+NAQuantum@users.noreply.github.com>
2018-12-07 07:42:46 -0800

    Small edits to file
    Removed some remnants from the .ipynb format and slightly optimized code by reducing the distance format to check if x^2+y^2<=1 rather than sqrt(x^2+y^2)<=1. More changes will likely follow.

--- Unit Circle Pi Approximation.py
+++ Unit Circle Pi Approximation.py
@@ -1,23 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from random import *
 import numpy as np
 import matplotlib.pyplot as plt
 def pair():
     return (random(),random())
-def dist(x):
+def dart(x):
     (a,b)=x
-    return np.sqrt(a**2+b**2)
-def dart():
-    return dist(pair())
-
-
-# In[ ]:
-
+    return (a**2+b**2)<=1.0
 
 # Dart creation and counting
 def add(darts,length):
@@ -34,10 +22,6 @@ def approx(darts):
 def error(darts):
     return abs(approx(darts)-np.pi)
 
-
-# In[ ]:
-
-
 # Iterations
 I = 7
 
@@ -47,11 +31,7 @@ errors = []
 for i in range(1,I+1):
     returns = add(returns,10**i-10**(i-1))
     errors += [error(returns)]
-
-
-# In[ ]:
-
-
+    
 # Simple generation for graph
 mylistylist = []
 for i in range(1,I+1):
