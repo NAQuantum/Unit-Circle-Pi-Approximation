8fca628212d2068c0f9caf09c0a7516e915101c2
NAQuantum <45130165+NAQuantum@users.noreply.github.com>
2018-12-07 09:22:23 -0800

    Improved code runtime, etc
    Improved the algorithm slightly (eg replacing long for loops with while loops and put more emphasis on counters) and fixed a few errors. There's a significant decrease in runtime -- I can actually run I=8 on my laptop without spending an eternity. Currently I is set to 5 because it runs very quickly and is a good number of approximations.

--- Unit Circle Pi Approximation.py
+++ Unit Circle Pi Approximation.py
@@ -3,43 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 def pair():
     return (random(),random())
-def dart(x):
-    (a,b)=x
+def dart():
+    (a,b)=pair()
     return (a**2+b**2)<=1.0
 
-# Dart creation and counting
-def add(darts,length):
-    for x in range(length):
-        darts += [dart()]
-    return darts
-def approx(darts):
-    c = 0
-    length = len(darts)
-    for i in range(length):
-        if darts[i]<=1.0:
-            c+=1
-    return 4*c/length
-def error(darts):
-    return abs(approx(darts)-np.pi)
-
 # Iterations
-I = 7
+# CHANGE THIS to determine the accuracy of the approximation
+I = 5
+
+def create(newdarts): # Generates newdarts more darts and returns how many fall inside the circle
+    i=newdarts
+    c=0
+    while i>0:
+        if dart():
+            c+=1
+        i-=1
+    return c
+
+def approx(circledarts,totaldarts): # Gives final approximation of pi
+    return 4*circledarts/totaldarts
+
+def error(circledarts, totaldarts):
+    return abs(approx(circledarts,totaldarts)-np.pi) # Objective error of approximation
 
 # Generation of approximations
-returns = [dart()]
+c = create(1)
 errors = []
-for i in range(1,I+1):
-    returns = add(returns,10**i-10**(i-1))
-    errors += [error(returns)]
+i=1
+while i<=I:
+    c+=create(10**i-10**(i-1))
+    errors+=[error(c,10**i)]
+    i+=1
     
-# Simple generation for graph
+# Generation of graph, don't touch this vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+# Getting the x-values list going
 mylistylist = []
 for i in range(1,I+1):
     mylistylist += [10**i]
-# Graph
+    
+# Actual graphing
 plt.xscale('log')
 plt.yscale('log')
 plt.plot(mylistylist, errors, 'o')
 plt.xlabel("Darts")
 plt.ylabel("Error in Approximation")
-
